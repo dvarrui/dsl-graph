7ffd5b69b791f3be1be8dda752abadf007258851
David Vargas <dvarrui@protonmail.com>
2026-04-21 14:27:45 +0100

    update

--- examples/1-create.rb
+++ examples/1-create.rb
@@ -3,14 +3,14 @@ require_relative "../lib/dsl/graph"
 
 puts "==> Create"
 
-graph do
+graph {
   n1 = add_node("Obiwan")
   n2 = add_node("Anakin")
   n3 = add_node("Yoda")
 
   add_edge(n1, n2, "maestro_de")
   add_edge(n3, n1, "maestro_de")
-end
+}
 
 puts "==> Debug"
 graph.debug
--- examples/2-export.rb
+++ examples/2-export.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 require_relative "../lib/dsl/graph"
 
-graph do
+graph {
   n1 = add_node("Obiwan")
   n2 = add_node("Anakin")
   n3 = add_node("Yoda")
 
   add_edge(n1, n2, "maestro_de")
   add_edge(n3, n1, "maestro_de")
-end
+}
 
 puts "==> Export"
 
